b056e9bf294ab9ede6623acf273d615086479ea8
usuario <alu0101392561@ull.edu.es>
2021-01-12 17:40:20 +0000

    Ejercicio Fecha

--- fecha/src/fecha.h
+++ fecha/src/fecha.h
@@ -1,3 +1,18 @@
+/**
+  * Universidad de La Laguna
+  * Escuela Superior de Ingeniería y Tecnología
+  * Grado en Ingeniería Informática
+  * Informática Básica
+  *
+  * @file fechas_funciones.cc
+  * @author José Lozano Armas alu0101392561@ull.edu.es
+  * @date 3-1-21
+  * @brief En este apartado se podrá encontrar las declaraciones del programa complejos_main.cc
+  * @bug 
+  * @see https://www.internalpointers.com/post/modern-cmake-beginner-introduction
+  *      https://www.learncpp.com/cpp-tutorial/81-welcome-to-object-oriented-programming/
+  *      
+  */
 #include <iostream>
 void Errores(int argc, char *argv[]);
 bool CondicionBisiesto(int numero_evaluar);
